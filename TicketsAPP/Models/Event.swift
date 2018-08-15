37ecbc2c3370613cc55d9be8a55835ebe0ab9aa4
Ricardo Hernandez <a.ricardohg@gmail.com>
2018-08-15 12:01:03 -0500

    added get events handler

--- TicketsAPP/Models/Event.swift
+++ TicketsAPP/Models/Event.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+private let eventEndpoint = "http://localhost:8080/events"
+
 struct Event: Codable {
     
     let id: Int
@@ -23,3 +25,35 @@ struct EventResponse: Codable {
     let events: [Event]
 }
 
+// API Calls Handler
+
+extension Event {
+    
+    typealias EventClosure = ([Event]?) -> ()
+    static func getEvents(limit: Int, offset: Int, completion: @escaping EventClosure) {
+        
+        guard let url = URL(string: eventEndpoint) else { return }
+        
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        let client = HTTPClient(session: session)
+        
+        client.get(url: url, parameters: ["limit":"\(limit)", "offset": "\(offset)"]) { (data, error) in
+            
+            guard let data = data else { return }
+            
+            do {
+                let decoder = JSONDecoder()
+                let eventsJson = try decoder.decode(EventResponse.self, from: data)
+                completion(eventsJson.events)
+                
+            }
+            catch {
+                completion(nil)
+                print(error)
+            }
+            
+        }
+        
+        
+    }
+}
--- TicketsAPP/Scenes/Events/Events/EventsTableViewController.swift
+++ TicketsAPP/Scenes/Events/Events/EventsTableViewController.swift
@@ -19,29 +19,12 @@ class EventsTableViewController: UITableViewController {
     }
     
     private func loadData() {
-        
-        guard let url = URL(string: "http://localhost:8080/events") else {
-            assertionFailure("wrong url")
-            return
-        }
-        
-        //let session = URLSession(configuration: URLSessionConfiguration.default)
-        let client = HTTPClient(session: URLSession.shared)
-        client.get(url: url, parameters: nil) { [weak self] (data, error) in
-            
-            guard let data = data, let s = self else { return }
-            
-            do {
-                let decoder = JSONDecoder()
-                let eventsJson = try decoder.decode(EventResponse.self, from: data)
-                s.events = eventsJson.events
-                DispatchQueue.main.async {
-                    s.tableView.reloadData()
-                }
                 
-            }
-            catch {
-                print(error)
+        Event.getEvents(limit: 2, offset: 0) {  [weak self] (events) in
+            guard let events = events, let s = self else { return }
+            s.events = events
+            DispatchQueue.main.async {
+                s.tableView.reloadData()
             }
         }
     }
